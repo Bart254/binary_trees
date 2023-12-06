5fe77af9fcf28a507e9b9d2b3558ad2500e3312b
Bart254 <omondibartlett@gmail.com>
2023-12-06 14:23:53 +0300

    update  14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,19 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a the tree
+ * binary_tree_height - measures the height of a node
  * @tree: pointer to the root node of the tree to measure height
- * Return: size of the node
+ * Return: height of the node
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
-	left_height = binary_tree_size(tree->left);
-	right_height = binary_tree_size(tree->right);
-	return (left_height + right_height + 1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (max(left_height, right_height) + 1);
+}
+
+/**
+ * max - returns the max between two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: max integer - 1
+ */
+int max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
 }
 
 /**
@@ -23,9 +38,13 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 int binary_tree_balance(binary_tree_t *tree)
 {
-	size_t left_tree_size, right_tree_size;
+	int left_tree_height, right_tree_height;
 
-	left_tree_size = binary_tree_size(tree->left);
-	right_tree_size = binary_tree_size(tree->right);
-	return (left_tree_size - right_tree_size);
+	left_tree_height = binary_tree_height(tree->left);
+	if (tree->left != NULL)
+		left_tree_height++;
+	right_tree_height = binary_tree_height(tree->right);
+	if (tree->right != NULL)
+		right_tree_height++;
+	return (left_tree_height - right_tree_height);
 }
